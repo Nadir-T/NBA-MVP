429140e59d348f0a2b3316bdab66d23e80338f18
coni26 <56028648+coni26@users.noreply.github.com>
2020-03-24 23:50:54 +0100

    Add files via upload

---
+++ NBA.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 24 23:37:23 2020
+
+@author: Coni
+"""
+
+import pandas as pd
+import numpy as np
+
+players = pd.read_csv('/Users/Coni/Desktop/ENSAE/1A/Projet informatique/Data/players.csv')
+seasons_stats = pd.read_csv('/Users/Coni/Desktop/ENSAE/1A/Projet informatique/Data/seasons_stats.csv')
