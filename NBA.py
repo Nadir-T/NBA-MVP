f2cb71aa733b5bfebd7468f72d219e2aa7ad7500
coni26 <56028648+coni26@users.noreply.github.com>
2020-03-24 23:55:17 +0100

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -9,5 +9,5 @@ Created on Tue Mar 24 23:37:23 2020
 import pandas as pd
 import numpy as np
 
-players = pd.read_csv('/Users/Coni/Desktop/ENSAE/1A/Projet informatique/Data/players.csv')
-seasons_stats = pd.read_csv('/Users/Coni/Desktop/ENSAE/1A/Projet informatique/Data/seasons_stats.csv')
+players = pd.read_csv('players.csv')
+seasons_stats = pd.read_csv('seasons_stats.csv')
