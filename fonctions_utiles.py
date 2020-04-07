4a5e6587edbc7a13d5f2be202c74eba44d8efe9c
coni26 <56028648+coni26@users.noreply.github.com>
2020-04-07 18:41:47 +0200

    Update fonctions_utiles.py

--- fonctions_utiles.py
+++ fonctions_utiles.py
@@ -46,3 +46,28 @@ def normalize_by_year(df):
         B = B[df.columns]   #On remet les colonnes dans l'ordre
         df_scaled = B.append(df_scaled)
     return df_scaled
+
+
+
+
+
+
+def assign_rank(l):
+    l_rank = []
+    for i in l:
+        buf = 1
+        for j in l:
+            if j>i:
+                buf+=1
+        l_rank.append([i,buf])
+    return l_rank
+
+def score_rank(y,y_p):
+    y_rank = assign_rank(y)
+    y_p_rank = assign_rank(y_p)
+    score = 0
+    for i in range(len(y_rank)):
+        if y_rank[i][0] != 0:
+            score += (y_rank[i][1]-y_p_rank[i][1])**2
+    return score
+  
