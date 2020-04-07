a62aa8c4ed0706e38feddc2b6d3db09dc1e76d6a
Le-N <62962427+Le-N@users.noreply.github.com>
2020-04-07 22:30:03 +0200

    Update fonctions_utiles.py

--- fonctions_utiles.py
+++ fonctions_utiles.py
@@ -70,4 +70,35 @@ def score_rank(y,y_p):
         if y_rank[i][0] != 0:
             score += (y_rank[i][1]-y_p_rank[i][1])**2
     return score
-  
+ 
+    
+### BATCH GRADIENT DESCENT ###
+
+def h(theta, X, n):
+    h = np.ones((X.shape[0],1))
+    theta = theta.reshape(1,n+1)
+    for i in range(0,X.shape[0]):
+        h[i] = float(np.matmul(theta, X[i]))
+    h = h.reshape(X.shape[0])
+    return h
+
+def BGD(theta, alpha, num_iters, h, X, y, n):
+    cost = np.ones(num_iters)
+    for i in range(0,num_iters):
+        theta[0] = theta[0] - (alpha/X.shape[0]) * sum(h - y)
+        for j in range(1,n+1):
+            theta[j] = theta[j] - (alpha/X.shape[0]) * sum((h-y) * X.transpose()[j])
+        h = hypothesis(theta, X, n)
+        cost[i] = (1/X.shape[0]) * 0.5 * sum(np.square(h - y))
+    theta = theta.reshape(1,n+1)
+    return theta, cost
+
+    #fonction à appeler en pratique : 
+def linear_regression(X, y, alpha, num_iters):
+    n = X.shape[1]
+    one_column = np.ones((X.shape[0],1))
+    X = np.concatenate((one_column, X), axis = 1)
+    theta = 0.1*np.random.randn(n+1)  #j'ai fait ce choix arbitraire peut-être à modifier
+    h = hypothesis(theta, X, n)
+    theta, cost = BGD(theta,alpha,num_iters,h,X,y,n)
+    return theta, cost
