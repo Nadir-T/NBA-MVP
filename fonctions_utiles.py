e557cb2fde880d8f3aea17a6ecaf52c657f76b93
coni26 <56028648+coni26@users.noreply.github.com>
2020-04-26 21:57:14 +0200

    Update fonctions_utiles.py

--- fonctions_utiles.py
+++ fonctions_utiles.py
@@ -1,20 +1,16 @@
 import pandas as pd
 import numpy as np
 from sklearn.utils import shuffle
+from sklearn.preprocessing import StandardScaler
 
 doss =  '/Users/Coni/Desktop/ENSAE/1A/Projet informatique/Data/'
 
 df = pd.read_csv(doss + 'df.csv')
-stats_clean = pd.read_csv(doss + 'stats_clean.csv')
-
-### Fonctions à faire ###
-# - normalise par années
-# - descente de gradient
-# - calcul du cout avec anomaly detection
-# - séparation train validation test
-# - il faut penser à supprimer ceux qui ont un tot
 
 def coupe(df):
+    """
+        Permet de diviser le dataset en deux datasets avec une proportion égale de MVP dans chaque dataset.
+    """
     prop = len(df[df['score MVP']>0])/len(df)
     mvp =  df[df['score MVP']>0]
     non_mvp =  df[df['score MVP']==0]
@@ -27,21 +23,25 @@ def coupe(df):
     test = pd.concat([mvp_test, non_mvp_test], ignore_index=True)
     train = pd.concat([mvp_train, non_mvp_train], ignore_index=True)
     return test, train
+    
 
-def coupe_by_year(df):
-    l = 1982 + np.random.choice(36, 1, replace=False)
+def coupe_by_year(df, n_years=1):
+    """
+        Permet de couper le dataset en deux dataset en fonction des années.
+        Le n_years permet de choisir de combien de saison on veut que notre dataset de test se compose.
+    """
+    l = 1982 + np.random.choice(36, n_years, replace=False)
     test = df[df['Year'].isin(l)]
     train = df[~df['Year'].isin(l)]
     return test, train
 
 
-#Normalisation par année
-
-from sklearn.preprocessing import StandardScaler
-
 scaler = StandardScaler() 
 
 def normalize_by_year(df):
+    """
+        Normalise les données année par année.
+    """
     df_scaled = pd.DataFrame(columns = df.columns)
     A = df.drop(['classé MVP','score MVP', 'Year'], axis = 1) #on enlève les colonnes qu'on ne normalise pas
     for k in range(1982, 2018):
@@ -54,64 +54,11 @@ def normalize_by_year(df):
         df_scaled = B.append(df_scaled)
     return df_scaled
 
-    
-### BATCH GRADIENT DESCENT ###
-
-def h(theta, X, n):
-    h = np.ones((X.shape[0],1))
-    theta = theta.reshape(1,n+1)
-    for i in range(0,X.shape[0]):
-        h[i] = float(np.matmul(theta, X[i]))
-    h = h.reshape(X.shape[0])
-    return h
-
-def BGD(theta, alpha, num_iters, h, X, y, n):
-    cost = np.ones(num_iters)
-    for i in range(0,num_iters):
-        theta[0] = theta[0] - (alpha/X.shape[0]) * sum(h - y)
-        for j in range(1,n+1):
-            theta[j] = theta[j] - (alpha/X.shape[0]) * sum((h-y) * X.transpose()[j])
-        h = hypothesis(theta, X, n)
-        cost[i] = (1/X.shape[0]) * 0.5 * sum(np.square(h - y))
-    theta = theta.reshape(1,n+1)
-    return theta, cost
-
-    #fonction à appeler en pratique : 
-def linear_regression(X, y, alpha, num_iters):
-    n = X.shape[1]
-    one_column = np.ones((X.shape[0],1))
-    X = np.concatenate((one_column, X), axis = 1)
-    theta = 0.1*np.random.randn(n+1)  #j'ai fait ce choix arbitraire peut-être à modifier
-    h = hypothesis(theta, X, n)
-    theta, cost = BGD(theta,alpha,num_iters,h,X,y,n)
-    return theta, cost
-
-
-
-
-def add_noise_mvp(train, Y_train):    # Y_train doit être une series si c'est déjà un DataFrame, enlever la première ligne
-    Y_train = pd.DataFrame(Y_train)
-    train['score MVP']=Y_train
-    train_mvp = train[train['score MVP']>0]
-    Y_train_mvp = Y_train[Y_train['score MVP']>0]
-    train_mvp = train_mvp.drop(['score MVP'], axis = 1)
-    mu, sigma = 0, 0.01 
-    train = train.drop(['score MVP'], axis = 1)
-    res_noise = train
-    res_Y = Y_train
-    for i in range(9):
-        noise = np.random.normal(mu, sigma, train_mvp.shape)
-        train_mvp_noise = train_mvp + noise
-        res_noise = pd.concat([res_noise, train_mvp_noise])
-        res_Y = pd.concat([res_Y, Y_train_mvp])    
-    res_Y = res_Y['score MVP']
-    return res_noise, res_Y
-
-
-
-### Score sur les rangs ###
 
 def assign_rank(l):
+    """
+        Assigne un rang à chaque élément de la liste.
+    """
     l_rank = []
     for i in l:
         buf = 1
@@ -122,6 +69,10 @@ def assign_rank(l):
     return l_rank
 
 def score_rank(y_rank,y_p_rank):
+    """
+        Renvoie le score (définit dans le rapport) entre deux listes, 1 si le premier est le bon, 
+        1 si le podium est le bon.
+    """
     score = 0
     pond = 0
     premier = 0
@@ -138,6 +89,33 @@ def score_rank(y_rank,y_p_rank):
             elif y_rank[i][1] == 3:
                 podium += (y_p_rank[i][1] == 3) * 1
     return score/pond, premier, (podium==3)*1
+    
+    
+def add_noise_mvp(train, Y_train, k=19):
+    """
+        Permet d'ajouter des données bruitées au dataset de train et au Y.
+        On rajoute le bruit uniquement sur les joueurs classés au MVP pour obtenir plus de MVP 
+        et ainsi leur donner plus d'importance.  Le paramètre k=19 permet d'avoir environ le 
+        même nombre de classés MVP que de non classés.
+    """
+    Y_train = pd.DataFrame(Y_train)
+    train['score MVP']=Y_train
+    train_mvp = train[train['score MVP']>0]
+    Y_train_mvp = Y_train[Y_train['score MVP']>0]
+    train_mvp = train_mvp.drop(['score MVP'], axis = 1)
+    mu, sigma = 0, 0.01          # mu = 0 est assez logique, et on a choisi sigma en observant les données.
+    train = train.drop(['score MVP'], axis = 1)
+    res_noise = train
+    res_Y = Y_train
+    for i in range(k):
+        noise = np.random.normal(mu, sigma, train_mvp.shape)
+        train_mvp_noise = train_mvp + noise
+        res_noise = pd.concat([res_noise, train_mvp_noise])
+        res_Y = pd.concat([res_Y, Y_train_mvp])    
+    res_Y = res_Y['score MVP']
+    return res_noise, res_Y
+
+
 
 
 
